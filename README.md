5f0f9c3bf5df3ad062247ad0c07ef5d2baf31736
Yetity <hlonipoole692@gmail.com>
2023-07-03 16:43:55 +0200

    Documentation

--- README.md
+++ README.md
@@ -1,2 +1,108 @@
+<style>
+    r {
+        color: red;
+    }
+</style>
+
 # djs-game
-A package that will help make discord.js bots have minigames!
+
+## Installation
+
+```
+npm i djs-game
+```
+
+## Game Class
+
+### Constructor
+
+#### Parameters
+
+-   `client` <r>(required)</r>: Your discord.js client object
+-   `id` <r>(required)</r>: The unique identifier for the game.
+
+```js
+const { Game } = require('djs-game');
+/* Discord.js code ahead
+
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent] });
+*/
+const game = new Game(client, 'gameId');
+```
+
+### Methods
+
+#### start
+
+Starts the game and emit's the "start" event.
+
+```js
+game.start();
+```
+
+#### isGameOn
+
+Returns game's current state
+
+```js
+console.log(game.isGameOn()); // false
+game.start();
+console.log(game.isGameOn()); // true
+```
+
+#### end
+
+Ends the game and emit's the "end" event.
+
+```js
+game.start();
+console.log(game.isGameOn()); // true
+game.end();
+console.log(game.isGameOn()); // false
+```
+
+#### handleButtons
+
+Handles button interactions during the game. Emits an event that is unique to the game ID when a button interaction occurs
+
+_Throws an error if the game is not started before handling buttons_
+
+```js
+game.handleButtons(); // Error: Cannot handle buttons before starting the game.
+game.start();
+game.handleButtons(); // void
+```
+
+### Example use
+
+```js
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+const game = new Game(client, 'game1');
+
+game.on('start', () => {
+    console.log('Game started!');
+});
+
+game.on('end', () => {
+    console.log('Game ended!');
+});
+
+game.on('error', (error) => {
+    console.error('An error occurred:', error);
+});
+
+game.start();
+
+game.handleButtons();
+```
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,46 +1,171 @@
 import { EventEmitter } from 'events';
 import { Client, Interaction } from 'discord.js';
 
+/**
+ * Represents a game instance.
+ */
 declare class Game extends EventEmitter {
+    /**
+     * Game variables.
+     */
     var: {
+        /**
+         * Game's state.
+         */
         gaming: boolean;
     };
+    /**
+     * Discord.js Client Instance.
+     */
     client: Client;
+    /**
+     * Game's Unique Identifier
+     */
     id: string;
 
+    /**
+     * Constructs a new Game instance.
+     * @param {Client} client A Discord.js client
+     * @param {string} id A unique game identifier
+     */
     constructor(client: Client, id: string);
+
+    /**
+     * Start's the game
+     */
     start(): void;
+
+    /**
+     * Returns true/false if game is on
+     */
     isGameOn(): boolean;
-    end(): void;
+
+    /**
+     * End's the game
+     * @param {Interaction} interaction Pass the interaction that was associated with the game. If null it will not do anythinng to the message.
+     */
+    end(interaction?: Interaction): void;
+
+    /**
+     * Forces the game to error and ends the game
+     * @param {string} error Error message
+     */
     error(error: string): void;
+
+    /**
+     * Handles button Interactions. can be placed beneath `game.start()`
+     */
     handleButtons(): void;
 
+    /**
+     * Listens for when the game starts
+     */
     on(event: 'start', listener: () => void): this;
+    /**
+     * Listens for when the game ends.
+     */
     on(event: 'end', listener: () => void): this;
+    /**
+     * Listens for when an error occurs
+     */
     on(event: 'error', listener: (error: TypeError) => void): this;
+    /**
+     * Listens for button interactions. ${string} is the button's ID
+     */
     on(
         event: `${string}-btn`,
         listener: (interaction: Interaction) => void
     ): this;
 }
 
+/**
+ * A 2d plane
+ */
 declare class Plane extends EventEmitter {
+    /**
+     *
+     * @param {Game} game Pass the game object
+     * @param {number} rows Amount of rows
+     * @param {number} columns Amount of columns
+     * @param {string|null} blank The string that represents a default character in the plane.
+     */
     constructor(
         game: Game,
         rows: number,
         columns: number,
         blank?: string | null
     );
-    createGrid(blank: string | null): string[][];
-    coordsAt(x: number, y: number): string | null | undefined;
+
+    /**
+     * The string that represents a default character in the plane.
+     */
+    blank: any;
+    /**
+     * The actual plane itself in it's raw form.
+     */
+    plane: string[][];
+    /**
+     * Plane's objects
+     */
+    objects: {
+        /**
+         * ID's of object cached on the plane. This is updated whenever the plane is updated.
+         */
+        ids: string[];
+        /**
+         * some planeobjects defined by the ids
+         */
+        objects: { [id: string]: PlaneObject };
+    };
+
+    /**
+     *
+     * @param {string} inputValue Lookup an objects id
+     * @returns {object|null} {x:?, y:?}
+     */
     lookupObj(inputValue: string): { x: number; y: number } | null;
+
+    /**
+     *
+     * @param {number} x X Coordinate
+     * @param {number} y Y Coordinate
+     * @returns {string|undefined}
+     */
     lookupCoords(x: number, y: number): string | undefined;
+
+    /**
+     * Clears the entire grid, removing every object. (Preserves object coordinates)
+     */
     clear(): void;
+
+    /**
+     *
+     * @param  {...PlaneObject} arr Objects to be updated on plane
+     */
     update(...arr: PlaneObject[]): void;
+
+    /**
+     *
+     * @param {string} row Split rows, default is null (assumes you use emojis for planes.)
+     * @param {string} column Split columns, default is a line break.
+     * @returns String with output plane.
+     */
     return(row?: string, column?: string): string;
 }
 
+/**
+ * An object to be represented on a plane
+ * @class
+ */
 declare class PlaneObject {
+    /**
+     *
+     * @param {number} x X coordinate, origin is (0) (top left hand corner) [Not zero idnexed]
+     * @param {number} y Y coordinate, origin is (0) (top left hand corner) [Not zero indexed]
+     * @param {string} id A unique object ID
+     * @param {string} value An emoji for said object to display on the plane. Defaults to objectID
+     * @param {boolean} detectCollision Detect collision on other Objects.
+     */
     constructor(
         x: number,
         y: number,
@@ -48,6 +173,27 @@ declare class PlaneObject {
         value?: string,
         detectCollision?: boolean
     );
+
+    /**
+     * X coordinate
+     */
+    x: number;
+    /**
+     * Y coordinate
+     */
+    y: number;
+    /**
+     *  Unique Identifier
+     */
+    id: string;
+    /**
+     * Value for it to return when displayed on the plane.
+     */
+    value: string;
+    /**
+     * Detect collision, if false, it will pass through existing PlaneObjects
+     */
+    detectCollision: boolean;
 }
 
 export { Game, Plane, PlaneObject };
